2f884080d2be61e5d8bbcc1a5b9727ff695296a4
tachyla <tachylad.murray@gmail.com>
2017-07-14 11:05:28 -0700

    VideoDetailViewController renders full screen playback image

--- Youtube-video-list/VideoDetailViewController.swift
+++ Youtube-video-list/VideoDetailViewController.swift
@@ -31,10 +31,15 @@ class VideoDetailViewController: UIViewController {
         if let vid = self.selectedVideo {
             self.titleLabel.text = vid.videoTitle
             self.descriptionLabel.text = vid.videoDescription
+            
+            let width = self.view.frame.size.width
+            let height = width/320 * 180
         
         
-        let videoEmbedString = "<html><head><style type=\"text/css\">body {background-color: transparent;color: white;}</style></head><body style=\"margin:0\"><iframe frameBorder=\"0\" height=\"180\" width=\"320\" src=\"http://www.youtube.com/embed/" + vid.videoId + "?showinfo=0&modestbranding=1&frameborder=0&rel=0\"></iframe></body></html>"
-   
+       /* let videoEmbedString = "<html><head><style type=\"text/css\">body {background-color: transparent;color: white;}</style></head><body style=\"margin:0\"><iframe frameBorder=\"0\" height=\"" + height +  "\" width=\""320\" src=\"http://www.youtube.com/embed/" + vid.videoId + "?showinfo=0&modestbranding=1&frameborder=0&rel=0\"></iframe></body></html>"
+   */
+            let videoEmbedString = "<html><head><style type=\"text/css\">body {background-color: transparent;color: white;}</style></head><body style=\"margin:0\"><iframe frameBorder=\"0\" height=\"" + String(describing: height) + "\" width=\"" + String(describing: width) + "\" src=\"http://www.youtube.com/embed/" + vid.videoId + "?showinfo=0&modestbranding=1&frameborder=0&rel=0\"></iframe></body></html>"
+            
          self.webView.loadHTMLString(videoEmbedString, baseURL: nil)
     }
     }
